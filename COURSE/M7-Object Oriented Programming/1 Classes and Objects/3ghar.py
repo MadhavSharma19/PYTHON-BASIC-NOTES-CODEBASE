f44b36f1bad1cd09ad459e8470faf436f8f398e8
MITS-MADHAV <madhav.sharma@mitssolution.com>
2026-05-21 12:47:13 +0530

    update

---
+++ COURSE/M7-Object Oriented Programming/1 Classes and Objects/3ghar.py
@@ -0,0 +1,16 @@
+class Ghar:
+
+    def __init__(self, a):
+
+        self.a = a
+
+    def gharbanao(self):
+
+        print("ghar ban gya")
+
+        print(f"number of floor = {self.a}")
+
+
+mistri = Ghar(3)
+
+mistri.gharbanao()
